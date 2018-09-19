043f79591d7b4bf40cca37bc15f6ed96c3f2ee34
Daan Kort <daankort3@gmail.com>
2018-09-19 15:42:47 +0200

    updated gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,20 +17,14 @@ gulp.task('sass:watch',  () => {
 gulp.task('serve', ['sass', 'lint'], () => {
 
     gulp.watch("../slider/scss/*.scss", ['sass']);
-	gulp.watch("../slider/*.html").on('change');
-	gulp.watch("../slider/*.css").on('change');
+    gulp.watch("../slider/*.html").on('change');
+    gulp.watch("../slider/*.css").on('change');
 });
 
 gulp.task('lint', () => {
-    return gulp.src(['js/*.js'])
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
+    return gulp.src(['js/*.js'])  
         .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
         .pipe(eslint.failAfterError());
 });
 
